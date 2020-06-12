bf9269aff2ab1fa35cee23584844ded8dfa35ff1
epaew <epaew.333@gmail.com>
2020-06-13 07:15:07 +0900

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts{,x}'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '__tests__/tsconfig.json',
+    },
+  },
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
+  testMatch: ['<rootDir>/__tests__/**/*.test.ts{,x}'],
 };
