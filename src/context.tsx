ca10ac2d7ba61e369d735924dc044082dd1bbd31
epaew <epaew.333@gmail.com>
2020-06-13 13:14:55 +0900

    Refactor src/index.tsx
    * Split codes.
    * Add createLiffContext() to overload typeof liff for TypeScript user.
    * Update type definitions.

---
+++ src/context.tsx
@@ -0,0 +1,81 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { liffStub as stub } from './liff-stub';
+import { Liff, LiffError } from './types';
+
+interface initalizeLiffProviderProps<T> extends LiffProviderProps<T> {
+  setError: React.Dispatch<React.SetStateAction<LiffError | undefined>>;
+  setLiff: React.Dispatch<React.SetStateAction<T>>;
+  setReady: React.Dispatch<React.SetStateAction<boolean>>;
+}
+interface LiffProviderProps<T> {
+  liffId: string;
+  stubEnabled?: boolean | Partial<T>;
+}
+interface LiffContext<T> {
+  error?: LiffError;
+  liff: T;
+  ready: boolean;
+}
+type CreateLiffContext = <T>() => {
+  LiffConsumer: React.Consumer<LiffContext<T>>;
+  LiffProvider: React.FC<LiffProviderProps<T>>;
+  useLiff: () => LiffContext<T>;
+};
+
+const initializeLiffProvider = <T extends any>({
+  liffId,
+  setError,
+  setLiff,
+  setReady,
+  stubEnabled,
+}: initalizeLiffProviderProps<T>) => {
+  return () => {
+    if (stubEnabled) {
+      if (typeof stubEnabled === 'object') setLiff({ ...stub, ...stubEnabled } as T);
+      setReady(true);
+      return;
+    }
+
+    window.liff
+      .init({ liffId })
+      .then(() => {
+        setLiff(window.liff as T);
+        setReady(true);
+      })
+      .catch(e => setError(e));
+  };
+};
+
+const createLiffProvider = <T extends any>(context: React.Context<LiffContext<T>>) => {
+  const LiffProvider: React.FC<LiffProviderProps<T>> = ({
+    children,
+    liffId,
+    stubEnabled = false,
+  }) => {
+    const [error, setError] = useState<LiffError>();
+    const [liff, setLiff] = useState<T>(stub as T);
+    const [ready, setReady] = useState(false);
+
+    useEffect(initializeLiffProvider({ liffId, setError, setLiff, setReady, stubEnabled }), [
+      window.liff,
+    ]);
+
+    return <context.Provider value={{ error, liff, ready }}>{children}</context.Provider>;
+  };
+
+  return LiffProvider;
+};
+
+export const createLiffContext: CreateLiffContext = <T extends any>() => {
+  const context = createContext<LiffContext<T>>({ liff: stub as T, ready: false });
+  context.displayName = 'LiffContext';
+
+  return {
+    LiffConsumer: context.Consumer,
+    LiffProvider: createLiffProvider(context),
+    useLiff: () => React.useContext(context),
+  };
+};
+
+const { LiffConsumer, LiffProvider, useLiff } = createLiffContext<Liff>();
+export { LiffConsumer, LiffProvider, useLiff };
---
+++ src/liff-stub.ts
@@ -0,0 +1,77 @@
+import { Liff } from './types';
+
+const bluetooth = {
+  getAvailability: async () => false,
+  requestDevice: async () => ({
+    id: 'id',
+    watchingAdvertisements: false,
+    watchAdvertisements: async () => {},
+    unwatchAdvertisements: () => {},
+    addEventListener: () => {},
+    removeEventListener: () => {},
+  }),
+};
+const permanentLink = {
+  createUrl: () => 'https://liff.line.me/liffId/path',
+  setExtraQueryParams: () => {},
+};
+
+export const liffStub: Liff = {
+  id: 'liffId',
+  init: async () => {},
+  ready: new Promise(() => {}),
+  getOS: () => 'web',
+  getLanguage: () => 'ja',
+  getVersion: () => '2.1.3',
+  getLineVersion: () => '10.8.0',
+  isInClient: () => true,
+  isLoggedIn: () => true,
+  isApiAvailable: () => true,
+  login: () => {},
+  logout: () => {},
+  getAccessToken: () => 'DummyAccessToken',
+  getIDToken: () => 'DummyIDToken',
+  getDecodedIDToken: () => ({
+    amr: [],
+    aud: 'aud',
+    email: 'email@example.com',
+    exp: 0,
+    iat: 0,
+    iss: 'iss',
+    name: 'name',
+    picture: 'picture_url',
+    sub: 'sub',
+  }),
+  getContext: () => ({
+    type: 'none',
+    viewType: 'full',
+    userId: 'userId',
+    availability: {
+      shareTargetPicker: {
+        permission: false,
+        minVer: '10.8.0',
+      },
+    },
+  }),
+  getProfile: async () => ({
+    displayName: 'displayName',
+    pictureUrl: 'https://example.com/test.jpg',
+    statusMessage: '',
+    userId: 'userId',
+  }),
+  getFriendship: async () => {
+    return { friendFlag: true };
+  },
+  permanentLink,
+  sendMessages: async () => {},
+  openWindow: ({ url, external }) => {
+    external ? window.open(url) : (window.location.href = url);
+  },
+  shareTargetPicker: async () => {},
+  scanCode: async () => ({ value: 'DummyCode' }),
+  closeWindow: () => {
+    window.close();
+  },
+  initPlugins: async () => {},
+  bluetooth,
+};
