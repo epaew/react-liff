b407983af7dee51944c11fd5daf4c9f0dfdc2bad
epaew <epaew@users.noreply.github.com>
2020-11-23 23:35:34 +0000

    deploy: 839ed80f41a1870d7617a56fb99ba55290a0b0c5

--- precache-manifest.475c5bfe04471468798b4cfe29f558b6.js
+++ precache-manifest.475c5bfe04471468798b4cfe29f558b6.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c2ac9da3742089043a724a6a7806a42b",
+    "revision": "cf7b7ab5fcc9ebbf2c8871d194ca6005",
     "url": "/react-liff/index.html"
   },
   {
-    "revision": "0de1f44e030012bd5d8f",
+    "revision": "a6717bf302cf376f03f4",
     "url": "/react-liff/static/css/main.d64cf8c2.chunk.css"
   },
   {
-    "revision": "5a02a73520ec307d5cc0",
-    "url": "/react-liff/static/js/2.bc7d336d.chunk.js"
+    "revision": "a3f2147e5bef5a41381e",
+    "url": "/react-liff/static/js/2.a47e297f.chunk.js"
   },
   {
-    "revision": "9a2d4b0c7089554a023103e086760f18",
-    "url": "/react-liff/static/js/2.bc7d336d.chunk.js.LICENSE.txt"
+    "revision": "d0260c04cab086e68947c40eff3d4c29",
+    "url": "/react-liff/static/js/2.a47e297f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0de1f44e030012bd5d8f",
-    "url": "/react-liff/static/js/main.a68cd668.chunk.js"
+    "revision": "a6717bf302cf376f03f4",
+    "url": "/react-liff/static/js/main.0fde608b.chunk.js"
   },
   {
     "revision": "64e7b6258e46f06dfabe",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-liff/precache-manifest.4b73be8c6d3e1e7667a06b3ddbe1425a.js"
+  "/react-liff/precache-manifest.475c5bfe04471468798b4cfe29f558b6.js"
 );
 
 self.addEventListener('message', (event) => {
