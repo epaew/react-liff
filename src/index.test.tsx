2e13b362f6975d63225c32b714b1557a9da5aabb
epaew <epaew.333@gmail.com>
2022-06-12 15:38:44 +0900

    Update src/context: use new API to get liff obj.

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,83 +1,56 @@
 import { default as liff } from '@line/liff';
 import { render, waitFor } from '@testing-library/react';
-import { FC } from 'react';
-
-import { Liff } from './types';
 
 import { LiffProvider, useLiff } from '.';
 
 jest.mock('@line/liff');
 
-const TestComponent: FC = () => {
-  const { error, isReady, liff } = useLiff();
+const TestComponent = () => {
+  const LiffConsumer = () => {
+    const { error, isReady, liff } = useLiff();
+
+    return (
+      <>
+        <p data-testid="isReady">{isReady.toString()}</p>
+        <p data-testid="error.message">{error instanceof Error && error.message}</p>
+        <p data-testid="liff.id">{liff?.id}</p>
+      </>
+    );
+  };
 
   return (
-    <>
-      <p data-testid="isReady">{isReady.toString()}</p>
-      <p data-testid="error.message">{error instanceof Error && error.message}</p>
-      <p data-testid="liff.id">{liff?.id}</p>
-    </>
+    <LiffProvider liffId={'myLiffId'}>
+      <LiffConsumer />
+    </LiffProvider>
   );
 };
 
 describe('LiffProvider', () => {
-  const tree = (stubEnabled?: boolean | Partial<Liff>) => (
-    <LiffProvider liffId={'myLiffId'} stubEnabled={stubEnabled}>
-      <TestComponent />
-    </LiffProvider>
-  );
-
-  describe('When stub is disabled', () => {
-    describe('When the `liff.init()` succeeds', () => {
-      it('shows myLiffId', async () => {
-        const { getByTestId } = render(tree());
-
-        await waitFor(() => {
-          expect(getByTestId('isReady').textContent).toBe('true');
-          expect(getByTestId('error.message').textContent).toBe('');
-          expect(getByTestId('liff.id').textContent).toBe('myLiffId');
-        });
-      });
-    });
-
-    describe('When the `liff.init()` failed', () => {
-      beforeEach(() => {
-        liff.init = jest.fn().mockImplementationOnce(async () => {
-          throw new Error('Failed to initialize liff.');
-        });
-      });
-
-      it('shows error message', async () => {
-        const { getByTestId } = render(tree(false));
-
-        await waitFor(() => {
-          expect(getByTestId('isReady').textContent).toBe('false');
-          expect(getByTestId('error.message').textContent).toBe('Failed to initialize liff.');
-        });
-      });
-    });
-  });
-
-  describe('When stub is enabled (true)', () => {
-    it('shows stubbed liffId', async () => {
-      const { getByTestId } = render(tree(true));
+  describe('When the `liff.init()` succeeds', () => {
+    it('shows myLiffId', async () => {
+      const { getByTestId } = render(TestComponent());
 
       await waitFor(() => {
         expect(getByTestId('isReady').textContent).toBe('true');
         expect(getByTestId('error.message').textContent).toBe('');
-        expect(getByTestId('liff.id').textContent).toBe('liffId');
+        expect(getByTestId('liff.id').textContent).toBe('myLiffId');
       });
     });
   });
 
-  describe('When stub is enabled (object)', () => {
-    it('shows overridden liffId', async () => {
-      const { getByTestId } = render(tree({ id: 'overriddenLiffId' }));
+  describe('When the `liff.init()` failed', () => {
+    beforeEach(() => {
+      liff.init = jest.fn().mockImplementationOnce(async () => {
+        throw new Error('Failed to initialize liff.');
+      });
+    });
+
+    it('shows error message', async () => {
+      const { getByTestId } = render(TestComponent());
 
       await waitFor(() => {
-        expect(getByTestId('isReady').textContent).toBe('true');
-        expect(getByTestId('error.message').textContent).toBe('');
-        expect(getByTestId('liff.id').textContent).toBe('overriddenLiffId');
+        expect(getByTestId('isReady').textContent).toBe('false');
+        expect(getByTestId('error.message').textContent).toBe('Failed to initialize liff.');
       });
     });
   });
